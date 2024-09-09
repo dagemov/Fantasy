68012c4f1fe165492a6af3665635d3a0f48bdb21
dagemov <sebcoss1234@gmail.com>
2024-09-09 08:33:03 -0400

    add Radzen to frontend

--- Fantasy.Frontend/Extensions/ServiceAppExtension.cs
+++ Fantasy.Frontend/Extensions/ServiceAppExtension.cs
@@ -1,5 +1,6 @@
 ﻿using Fantasy.Frontend.Repositories.Interfaces;
 using Fantasy.Frontend.Repositories.Service;
+using Radzen;
 
 namespace Fantasy.Frontend.Extensions;
 
@@ -7,6 +8,12 @@ public static class ServiceAppExtension
 {
     public static IServiceCollection AddServiceApp(this IServiceCollection services, IConfiguration config)
     {
+        // Registrando el servicio ThemeService y otros servicios de Radzen
+        services.AddScoped<ThemeService>();
+        services.AddScoped<DialogService>();
+        services.AddScoped<NotificationService>();
+        services.AddScoped<TooltipService>();
+        services.AddScoped<ContextMenuService>();
         // cuidadon con el / al final para rutear
         services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7231/") });
 
